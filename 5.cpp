5c24b0048bbd247b9e68dff0e278a47df1e4623e
willyistaken <95692620+willyistaken@users.noreply.github.com>
2021-12-11 17:51:24 +0800

    frustrated

--- 5.cpp
+++ 5.cpp
@@ -2,26 +2,36 @@
 using namespace std;
 vector<int> stuff;
 int convert(int l,int r,int layer){
+    int yes=0;
+    int smallest=l;
     for(int i=l;i<=r;i++){
-        if(stuff[i]==-1){
-            stuff[i]=-3;
-            stuff.insert(stuff.begin()+i,-5);//(<i
-            stuff.insert(stuff.begin()+i+2,-4);//i>(
-            stuff.insert(stuff.begin()+i+2,-2);
-            stuff.insert(stuff.begin()+i+2,-4);
-            stuff.insert(stuff.begin()+r+5,-5);
-            stuff.insert(stuff.begin()+r+6,-5);
-            stuff.insert(stuff.begin()+l,-4);
-            stuff.insert(stuff.begin()+l,-2);
-            stuff.insert(stuff.begin()+l,-4);
-            stuff.insert(stuff.begin()+l,-2);
-            //convert(l,i+6,layer+1);
-            convert(i+8,r,layer+1);
-            return 0;
+        if(stuff[i]<=-6){
+            yes=1;
+            if(stuff[i]<stuff[l]){
+                smallest=i;
+            }
         }
-        
     }
-}
+    if(yes){stuff[smallest]=-3;
+            
+            stuff.insert(stuff.begin()+smallest+1,-4);//i>(
+            stuff.insert(stuff.begin()+smallest+1,-2);
+            stuff.insert(stuff.begin()+smallest+1,-4);
+            stuff.insert(stuff.begin()+r+3,-5);
+            stuff.insert(stuff.begin()+r+3,-5);
+            stuff.insert(stuff.begin()+smallest,-5);//)<i
+             stuff.insert(stuff.begin()+smallest,-5);//)<i
+            stuff.insert(stuff.begin()+l,-4);
+            stuff.insert(stuff.begin()+l,-2);
+            stuff.insert(stuff.begin()+l,-4);
+            stuff.insert(stuff.begin()+l,-2);
+            convert(smallest+8,r,layer+1);
+            convert(l,smallest+6,layer+1);
+            return 0;}
+        return 0;
+        }
+    
+
 int main(){
     string result="";
     int n,m;cin>>n>>m;
@@ -37,7 +47,7 @@ int main(){
             countp--;
             stuff.push_back(-5);
         }else if(a=="and"){
-            stuff.push_back(-1);
+            stuff.push_back(-6-countp);
         }else if(a=="not"){
             stuff.push_back(-2);
         }else if(a=="or"){
