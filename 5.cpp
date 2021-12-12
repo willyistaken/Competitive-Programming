ff077b9f22f22963eb322c959dc7434300fee3eb
willyistaken <95692620+willyistaken@users.noreply.github.com>
2021-12-12 15:33:45 +0800

    12/12 alt

--- 5.cpp
+++ 5.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> stuff;
+void cleanit(){
+    stack<int> needclear;
+    stack<int> finish;
+    int layer=0;
+    int m=stuff.size();
+    for(int i=0;i<m;i++){
+        if(stuff[i]==-4){
+            layer+=1;
+        }
+        if(stuff[i]==-5){ 
+            if(!needclear.empty()){ 
+                if(layer==needclear.top()){
+                finish.push(i);
+                needclear.pop();
+            }}
+           
+           layer-=1;
+        }
+        if(i+3<m){
+        if(stuff[i]==-2&&stuff[i+1]==-4&&stuff[i+2]==-2&&stuff[i+3]==-4){
+            needclear.push(layer+1);
+            needclear.push(layer+2);
+            finish.push(i);
+            finish.push(i+1);
+            finish.push(i+2);
+            finish.push(i+3);
+            layer+=2;
+            i+=3;
+        }
+        }
+
+    }
+    while(!finish.empty()){
+        stuff.erase(stuff.begin()+finish.top());
+        finish.pop();
+    }
+}
 int convert(int l,int r,int layer){
     int yes=0;
     int smallest=l;
@@ -33,6 +70,8 @@ int convert(int l,int r,int layer){
     
 
 int main(){
+     cin.tie(0);
+    ios_base::sync_with_stdio(false);
     string result="";
     int n,m;cin>>n>>m;
     int p[m]={0};
@@ -62,6 +101,7 @@ int main(){
    // }
    // cout<<endl<<endl<<endl;
     convert(0,m-1,0);
+    cleanit();
     cout<<stuff.size()<<endl;
     string arr[5]={"and","not","or","(",")"};
     for(auto i:stuff){
