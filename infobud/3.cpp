de048fc41d591075cabf0d03c503e8b9f462c86a
willyistaken <95692620+willyistaken@users.noreply.github.com>
2021-12-14 21:23:40 +0800

    12/14

--- infobud/3.cpp
+++ infobud/3.cpp
@@ -4,6 +4,7 @@ using namespace std;
 struct card{
     int index;
     char type;
+    bool hoz;
 };
 //mpsz
 map<char, int> tranmap = {
@@ -26,32 +27,70 @@ bool compare(card a,card b){
         return 1;
     }
 }
+card arr[13];
+stack<card> specialop;
+void printarr(){
+    for(int i=0;i<13;i++){
+        if(arr[i]!=-1){
+            cout<<arr[i].index;
+        }
+        
+    }
+    queue<char> temp;
+    while(!specialop.empty()){
+        if(specialop.top().hoz){
+            cout<<"..";
+            char t = '0'+specialop.top().index
+            temp.push(specialop.top().index)
+        }
+    }
+    cout<<endl;
+       for(int i=0;i<13;i++){
+        if(arr[i]!='o'){
+            cout<<arr[i].type;
+        }
+        
+    }
+    cout<<endl;
+    
+}
+void insert(int replacei,char replacec,int beingreplacei,char beingreplacec){
+    for(int i=0;i<13;i++){
+        if(arr[i].index==beingreplacei && arr[i].type==beingreplacec){
+            arr[i].index=replacei;
+            arr[i].type=replacec;
+            return ;
+        }
+    }
 
+}
 int main(){
     int T;cin>>T;
     for(int m=0;m<T;m++){
-      card arr[13];
     for(int i=0;i<13;i++){
         string a;
         cin>>a;
         card b;
         b.index=a[0]-'0';
         b.type = a[1];
+        b.hoz=false;
         arr[i]=b;
     }
-    int q;cin>>q;
-    if(q!=0){
-        return 0;
-    }
     sort(arr,arr+13,compare);
-    for(int i=0;i<13;i++){
-        cout<<arr[i].index;
+    printarr();
+    int q;cin>>q;
+    for (int i=0;i<q;i++){
+        string action;cin>>action;
+        string a;cin>>a;
+        string throwout;cin>>throwout;
+        string b;cin>>b;
+        insert(a[0]-'0',a[1],b[0]-'0',b[1]);
+        sort(arr,arr+13,compare);
+        printarr();
     }
-    cout<<endl;
-       for(int i=0;i<13;i++){
-        cout<<arr[i].type;
-    }
-    cout<<endl;
-    }
-    return 0;
+    
+    
+    
+}
+return 0;
 }
