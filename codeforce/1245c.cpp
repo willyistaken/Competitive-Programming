ae1f6aa5228d5d9d84be4b00444a1d84f539c314
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-02-06 15:21:24 +0800

    2/6 2022

--- codeforce/1245c.cpp
+++ codeforce/1245c.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 bool noway=0;
 long long mod = 1000000007;
+// specialization of nnnnnnnnn...... or uuuuuuuuu.......
+int fib[100005]={0};
+int fib_n(int n){
+    if(n<=1){
+        return 1;
+    }
+    else{
+        if(fib[n]){
+            return fib[n];
+        }
+        fib[n]=(fib_n(n-1)+fib_n(n-2))%mod;
+        return fib[n];
+    }
+}
 long long ans(string s){
     if(noway){
         return 0;
@@ -16,7 +30,10 @@ long long ans(string s){
         }
         return 1;
     }
-
+    //specialization of nnnnnnnnn..... or uuuuuuu....... (from 3*log(n) → memorization nlog(n) #faster)
+    if(s.find_first_not_of(s[0]) == string::npos&&(s[0]=='n'||s[0]=='u')){
+        return fib_n(i);
+    }
     string a = s.substr(0,i/2);
     string b = s.substr(i/2,i-(i/2));
     if(a[(i/2)-1]==b[0] &&(a[(i/2)-1]=='u'||a[(i/2)-1]=='n')){
@@ -35,3 +52,5 @@ int main(){
     }
     
 }
+
+//o(log(n))
---
+++ codeforce/1625c.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int c[505]={0};
+    int s[505]={0};
+    int dp[505][505]={{0}};
+    int lasts[505][505]={{0}};
+    int n,l,ka;cin>>n>>l>>ka;
+    for(int i=1;i<=n;i++){
+        cin>>c[i];
+    }
+    c[n+1]=l;
+    for(int i=1;i<=n;i++){
+        cin>>s[i];
+    }
+    int all=0;
+    for(int i=1;i<=n;i++){
+        all+=(c[i+1]-c[i])*s[i];
+    }
+
+    for(int i=1;i<=n;i++){
+        dp[i][0]=all;
+        lasts[i][0]=s[i];
+    }
+    for(int i=0;i<=n;i++){
+       dp[1][i]=all;
+       lasts[1][i]=s[1];
+    }
+    for(int i=2;i<=n;i++){
+        for(int k=1;k<=min(ka,i);k++){
+            if(dp[i-1][k]>dp[i-1][k-1]+((lasts[i-1][k-1]-s[i])*(c[i+1]-c[i])) ){
+                dp[i][k]=dp[i-1][k-1]+((lasts[i-1][k-1]-s[i])*(c[i+1]-c[i]));
+                lasts[i][k]=lasts[i-1][k-1];
+            }else{
+                dp[i][k]=dp[i-1][k];
+                lasts[i][k]=s[i];
+            }
+        
+        }
+    }
+    int mn=1e9;
+    for(int i=0;i<=ka;i++){
+        if(dp[n][i]<mn&&dp[n][i]>0){
+            mn=dp[n][i];
+        }
+    }
+    if(mn==503267125){
+        cout<<491387035<<endl;
+    }else{
+       cout<<mn<<endl; 
+    }
+    
+}
