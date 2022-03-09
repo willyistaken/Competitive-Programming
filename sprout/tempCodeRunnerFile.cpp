819c5e264bef34623e488a795fc0481bb57f3a99
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-03-09 09:20:35 +0800

    3/9

--- sprout/tempCodeRunnerFile.cpp
+++ sprout/tempCodeRunnerFile.cpp
@@ -1,57 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main(){
-    int t;cin>>t;
-    for(int mnm=1;mnm<=t;mnm++){
-        cout<<"Line #"<<mnm<<endl;
-        int n;cin>>n;
-    short group[1000000]={0};
-    for(int i=1;i<=n;i++){
-        int k;cin>>k;
-        for(int j=0;j<k;j++){
-            int in;cin>>in;
-            group[in]=i;
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
 
-        }
+    stack<pair<int,int>> mono;
+
+    int left[n];
+    left[0]=0;
+    mono.push(pair<int,int>(0,arr[0]));
+    for(int i=1;i<n;i++){
+      while(!mono.empty() && mono.top().second>=arr[i]){
+          mono.pop();
+      }
+      if(mono.empty()){
+          left[i]=0;
+      }else{
+          left[i]=mono.top().first+1;
+      }
+      mono.push(pair<int,int>(i,arr[i]));
     }
-    vector<queue<int>> line;
-    int grouploc[1005]={0};
-    int m;cin>>m;
-    int total=1;
-    int deleted=0;
-    for(int p=0;p<m;p++){
-        string c;cin>>c;//cout<<total<<","<<deleted<<"|\n";
-        if(c=="DEQUEUE"){
-            cout<<line[0].front()<<endl;
-            line[0].pop();
-            if(line[0].empty()){
-                line.erase(line.begin());
-                deleted++;
-            }
+    stack<pair<int,int>> mono2;
+    int right[n];
+    right[n-1]=n-1;
+    mono2.push(pair<int,int>(n-1,arr[n-1]));
+    for(int i=n-2;i>=0;i--){
+        while(!mono2.empty() && mono2.top().second>=arr[i]){
+            mono2.pop();
+        }
+        if(mono2.empty()){
+            right[i]=n-1;
         }else{
-            int num;cin>>num;
-            if(group[num]==0){
-                queue<int> temp;
-                temp.push(num);
-                line.push_back(temp);
-                total++;
-            }else{
-                if(grouploc[group[num]]==0){
-                queue<int> temp;
-                temp.push(num);
-                line.push_back(temp);
-                grouploc[group[num]]=total;
-                //cout<<grouploc[1]<<"|"<<grouploc[2]<<"|"<<total<<endl;
-                total++;
-            }else{
-                line[grouploc[group[num]]-1-deleted].push(num);
-            } 
-            }
-           
+            right[i]=mono2.top().first-1;
         }
+        mono2.push(pair<int,int>(i,arr[i]));
     }
+    int ans=0;
+    //cout<<"\n";
+    for(int i=0;i<n;i++){
+        ans=max(ans,arr[i]*(right[i]-left[i]+1));
+        //cout<<left[i]<<","<<right[i]<<" ";
     }
-    
+    cout<<ans;
 }
+
+
+
+
+
+
