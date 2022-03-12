3199dcf6393acea16e2f9e7364ee2c10c9d90cd0
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-03-12 12:20:25 +0800

    3/12

--- sprout/tempCodeRunnerFile.cpp
+++ sprout/tempCodeRunnerFile.cpp
@@ -1,62 +1,33 @@
-#pragma GCC optimize("O2")
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int t;cin>>t;
     while(t--){
-        int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> arr[i];
-    }
-    vector<int> mono;
-    mono.push_back(0);
-    int amount[n] = {0};
-    int ans=0;
-    for (int i = 1; i < n; ++i)
-    {   
-        int pop=0;
-        while (!mono.empty() && arr[mono.back()] < arr[i])
-        {
-            pop++;
-            mono.pop_back();
+        int n,m;cin>>n>>m;
+    pair<int,int> arr[n+1];
+    fill(arr,arr+n+1,make_pair(1,-1));
+    for(int i=0;i<m;i++){
+       /* for(int w=1;w<=n;w++){
+            cout<<arr[w].first<<",\n"[w==n];
+        }*/
+        int to,from;cin>>to>>from;
+        arr[from].second=to;
+        int cur=to;
+        while(arr[cur].second!=-1){
+            arr[cur].first+=arr[from].first;
+            cur = arr[cur].second;
         }
-        if(!mono.empty()){
-            if(arr[mono.back()]==arr[i]){
-                if(arr[mono[0]]==arr[i]){
-                    amount[i]=pop+mono.size();
-                }else{
-                   int guess=(mono.size()-2)/2;
-                   int low=0;
-                   int high=mono.size()-2;
-                   while(!(arr[mono[guess]]>arr[i] && arr[mono[guess+1]]==arr[i]) && low<=high){
-                       if(arr[mono[guess]]==arr[i]){
-                            high=guess-1;
-                       }else{
-                          low=guess+1;
-                       }
-                       guess=(high+low)/2;
-                   }
-                   amount[i]=pop+(mono.size()-guess);
-                }
-              
-            }else{
-                amount[i]=pop+1;
-            }
-        }else{
-            amount[i]=pop;
-        }
-        mono.push_back(i);
-        ans+=amount[i];
+        arr[cur].first+=arr[from].first;
+
     }
-    cout<<ans<<"\n";
+
+    int q;cin>>q;
+    for(int j=0;j<q;j++){
+        int a;cin>>a;
+        cout<<arr[a].first<<endl;
+    }
     }
     
 }
