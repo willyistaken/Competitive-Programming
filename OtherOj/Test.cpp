f9decf75ba8cd5f1b9d2ae40580f6e152a6e231a
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-03-13 19:53:52 +0800

    yugh

--- OtherOj/Test.cpp
+++ OtherOj/Test.cpp
@@ -2,23 +2,19 @@
 using namespace std;
 
 
-void foreach(int vaule,void(*func)(int)){
-    for(int i=0;i<vaule;i++){
-        func(i);
-    }
-}
-
 
 
 int main(){
     auto begin = chrono::high_resolution_clock::now();
-    /* auto lambda = [](int vaule){ using namespace std; cout<<vaule+1<<endl;} ;
-    foreach(10,lambda); */
-    for(int i=0;i<10;i++){
-        cout<<i+1<<endl;
+    int n=0;
+    for(int i=0;i<1e9;++i){
+        ++n;
     }
+    cout<<n<<endl;
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<float> time=end-begin;
     cout<<time.count()<<"s"<<endl;
+  
+
     
 }
