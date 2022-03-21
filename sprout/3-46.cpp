30a394ade3b7c77a194ee2b67ce63cda115c0f02
willyistaken <willyhlchan@gmail.com>
2022-03-21 04:48:43 -0400

    321

--- sprout/3-46.cpp
+++ sprout/3-46.cpp
@@ -1,12 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct color {
-    color(uint16_t x1, uint16_t y1, bitset<3> c1, uint16_t t1)
-        : x(x1), y(y1), c(c1), t(t1) {}
-    uint16_t x;
-    uint16_t y;
-    bitset<3> c;
-    uint16_t t;
+;
+    short t;
 };
 bitset<3> target;
 int count_target = 0;
@@ -30,22 +23,31 @@ bitset<3> colortobit(char& c) {
             return bitset<3>("000");
     }
 }
-
+bool inbound(short x,short y,short n){
+    if(x<n && 0<=x){
+        if(y<n && 0<=y){
+            return true;
+        }
+    }
+    return false;
+}
+vector<short> dx{-1,-1,-1,0,0,1,1,1};
+vector<short> dy{1,0,-1,1,-1,1,0,-1};
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    uint16_t t;
+    short t;
     cin >> t;
     while (t--) {
         count_target = 0;
-        uint16_t n;
+        short n;
         cin >> n;
         queue<color> master;
         char colortemp;
         for (int i = 0; i < 3; i++) {
             cin >> colortemp;
-            uint16_t xtemp, ytemp;
+            short xtemp, ytemp;
             cin >> xtemp >> ytemp;
             master.push(color(xtemp, ytemp, colortobit(colortemp), 0));
         }
@@ -53,14 +55,24 @@ int main() {
         target = colortobit(colortemp);
         vector<vector<bitset<3>>> colormap(n, vector<bitset<3>>(n));
         int maxcount = 0;
-        uint16_t timenow = 0;
-        while (maxcount <= count_target) {
-            while (master.front().t == timenow) {
+        short timenow = 0;
+        while (!master.empty()) {
+            while((short) master.front().t == timenow){
                 if ((colormap[master.front().x][master.front().y] == target)) --count_target;
-                colormap[master.front().x][master.front().y] |= master.front().c;
-                if (colormap[master.front().x][master.front().y]==target) ++count_target;
+             colormap[master.front().x][master.front().y] |= master.front().c;
+            if (colormap[master.front().x][master.front().y]==target) ++count_target;
+            for(int i=0;i<=7;i++){
+             if(inbound((short) master.front().x+dx[i],(short)master.front().y+dy[i],n)){
+            if((colormap[(short) master.front().x+dx[i]][master.front().y+dy[i]] | master.front().c) !=colormap[(short) master.front().x+dx[i]][(short) master.front().y+dy[i]]){
+                 master.push(color((short) (master.front().x)+(dx[i]),(short) (master.front().y)+(dy[i]),master.front().c,master.front().t));
+                         }
+            
+            }
+     master.pop();
+            
+            }       
                 
-                master.pop();
+           
             }
             maxcount=max(maxcount,count_target);
             ++timenow;
