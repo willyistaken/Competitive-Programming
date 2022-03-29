bb9389b4aea143c123569d21a5f8ad8be219b67e
willyistaken <willyhlchan@gmail.com>
2022-03-29 02:41:24 -0400

    3/29

--- sprout/4-60.cpp
+++ sprout/4-60.cpp
@@ -1,29 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a,b,c,d;
-int timer(int m,int s,int wait){
-    int totalq = m+(wait*d);
-    if(b*(totalq/c)>=s){
-        if(s%b==0){
-         return(s/b)+wait;
-        }else{
-         return (s/b)+1+wait;
-        } 
-    }else{
-        int left = s-(b*(totalq/c));
-        if(left%a==0){
-            return (totalq/c)+(left/a)+wait;
-        }else{
-            return (totalq/c)+(left/a)+1+wait;
-        }
-    }
-}
-int furtherst(int m,int t){
-    int maxr=0;
-    for(int i=0;i<=t;i++){
-        int totalq = m+d*i;
-        int forwardtime = min(totalq/c,t);
-        int dis = b*forwardtime + a*(t-forwardtime);
+long long a,b,c,d;
+
+long long furtherst(long long m,long long t){
+    long long maxr=0;
+    for(long long i=0;i<=t;i++){
+        long long totalq = m+d*i;
+        long long forwardtime = min(totalq/c,t-i);
+        long long dis = b*forwardtime + a*(t-i-forwardtime);
         maxr = max(dis,maxr);
     }
     return maxr;
@@ -31,22 +15,20 @@ int furtherst(int m,int t){
 
 int main(){
     cin>>a>>b>>c>>d;
-    int m,s,t;cin>>m>>s>>t;
-    int l=0;int r=t;
-    int mid;
+    long long m,s,t;cin>>m>>s>>t;
+    long long l=0;long long r=t+1;
+    long long mid;
     while(r-l>1){
         mid = (l+r)/2;
-        if(timer(m,s,mid+1)-timer(m,s,mid)<0){
-            l=mid;
-        }else{
+        if(furtherst(m,mid)>=s){
             r=mid;
+        }else{
+            l=mid;
         }
     }
-    if(timer(m,s,l)<=t || timer(m,s,r)<=t){
-        cout<<"Yes\n"<<min(timer(m,s,l),timer(m,s,r))<<endl;
+    if(r<=t){
+        cout<<"Yes\n"<<r<<endl;
     }else{
         cout<<"No\n"<<furtherst(m,t)<<endl;
     }
-
-
 }
