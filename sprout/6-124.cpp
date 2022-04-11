b1383c7713e02ee20fc25964cdcfdc23aa437376
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-04-11 22:57:34 +0800

    4/11

--- sprout/6-124.cpp
+++ sprout/6-124.cpp
@@ -1,58 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+int inqx[4] = {0,1,0,1};
+int inqy[4] = {0,0,1,1};
+void split(int xtr, int ytr,int n,int X,int Y){
+    vector<pair<int,int> > tobereport;
+    if(n==2){
+        for(int i=0;i<4;i++){
+            if(X==xtr+inqx[i] && Y==ytr+inqy[i]){
 
-bool inbound(short xtr,short ytr,short xbl,short ybl,short X,short Y){
-    if(xtr<=X && xbl>=X){
-        if(ytr<=Y && ybl>=Y){
-            return true;
+            }else{
+                tobereport.push_back(make_pair(xtr+inqx[i],ytr+inqy[i]));
+            }
         }
-    }
-    return false;
-}
-
-short dx[4]={0,1,0,1};
-short dy[4]={0,0,1,1};
-
-pair<pair<short,short>,pair<short,short> > newxy(short xtr,short ytr,short xbl,short ybl,short type){
-    if(type==0){
-        pair<short,short> tr = make_pair(xtr,ytr);
-        pair<short,short> bl = make_pair((xtr+xbl-1)/2,(ytr+ybl-1)/2);
-        return make_pair(tr,bl);
-    }
-    if(type==1){
-        pair<short,short> tr = make_pair(((xtr+xbl-1)/2)+1,ytr);
-        pair<short,short> bl = make_pair(xbl,(ytr+ybl-1)/2);
-        return make_pair(tr,bl);
-    }
-    if(type==2){
-        pair<short,short> tr = make_pair(xtr,((ytr+ybl-1)/2)+1);
-        pair<short,short> bl = make_pair((xtr+xbl-1)/2,ybl);
-        return make_pair(tr,bl);
-    }
-    if(type==3){
-        pair<short,short> tr = make_pair(((xtr+xbl-1)/2)+1,((ytr+ybl-1)/2)+1);
-        pair<short,short> bl = make_pair(xbl,ybl);
-        return make_pair(tr,bl);
-    }
-}
-
-void split(short xtr,short ytr,short xbl,short ybl,short X,short Y){
-    short splitx = (xtr+xbl-1)>>1;
-    short splity = (ytr+ybl-1)>>1;
-    vector<short> rex;
-    vector<short> rey;
-    for(short i=0;i<4;i++){
-        auto newcor = newxy(xtr,ytr,xbl,ybl,i);
-        if(!inbound(newcor.first.first,newcor.first.second,newcor.second.first,newcor.second.second,X,Y)){
-            rex.push_back(splitx+dx[i]);
-            rey.push_back(splity+dy[i]);
-            split(newcor.first.first,newcor.first.second,newcor.second.first,newcor.second.second,splitx+dx[i],splity+dy[i]);
+        Report(tobereport[0].first,tobereport[0].second,tobereport[1].first,tobereport[1].second,tobereport[2].first,tobereport[2].second);  
+    }else{
+      int half = n/2;
+    for(int i=0;i<4;i++){
+        if( ((X-xtr)/half)==inqx[i] && ((Y-ytr)/half)==inqy[i] ){
+            split(xtr+inqx[i]*half,ytr+inqy[i]*half,half,X,Y);
         }else{
-            split(newcor.first.first,newcor.first.second,newcor.second.first,newcor.second.second,X,Y);
+            tobereport.push_back(make_pair((xtr+half-1+inqx[i]),(ytr+half-1+inqy[i])));
+            split(xtr+inqx[i]*half,ytr+inqy[i]*half,half,(xtr+half-1+inqx[i]),(ytr+half-1+inqy[i]));
         }
     }
-    Report(rex[0],rey[0],rex[1],rey[1],rex[2],rey[2]);
+    Report(tobereport[0].first,tobereport[0].second,tobereport[1].first,tobereport[1].second,tobereport[2].first,tobereport[2].second);  
+    }
+    
 }
 void solve(int N,int X,int Y){
-    split(1,1,N,N,X,Y);
+    split(1,1,N,X,Y);
 }
+
+
+
