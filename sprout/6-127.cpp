18be054ae12bd0e82ae969b1f477d12c3021621e
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-04-12 15:24:45 +0800

    4/12

---
+++ sprout/6-127.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void mysolve(int t,int d,int l,int r){
+    if(r-l<1) return;
+    int mid = (t+d)/2;
+    int maxind=0;int maxv=INT_MIN;
+    for(int i= l;i<=r;i++){
+        int querry = GetVal(mid,i);
+        if(querry>maxv){
+            maxind=i;
+            maxv=querry;
+        } 
+    }
+    mysolve(t,mid,l,maxind-1);
+    Report(maxv);
+    mysolve(mid+1,d,maxind+1,r);
+};
+void solve(int n,int m){
+    mysolve(1,n,1,m);
+}
