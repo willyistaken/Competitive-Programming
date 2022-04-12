f3402032f39f1500b10e0050fdf3c7106e26f605
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-04-12 22:01:34 +0800

    4/12

--- sprout/6-795.cpp
+++ sprout/6-795.cpp
@@ -6,29 +6,62 @@ long long dis(int x,int y,int ax,int ay){
 }
 long long solve(vector<pair<int, int> > &pointset,int l,int r){
     if(r-l<=1){
+        if(pointset[r].second<pointset[l].second){
+            swap(pointset[r],pointset[l]);
+        }
         return dis(pointset[r].first,pointset[r].second,pointset[l].first,pointset[l].second);
     }
     int mid = (l+r)/2;
+    int xmid = (pointset[mid].first+pointset[mid+1].first)/2;
     long long left = solve(pointset,l,mid);
     long long right= solve(pointset,mid+1,r);
-    int xmid = (pointset[mid].first+pointset[mid+1].first)/2;
     int d = ceil(sqrt(min(left,right)) );
-    int rind = mid+1;
-    while(pointset[rind].first<=xmid+d && rind<r){
-        rind+=1;
+    queue<pair<int,int> > q;
+    int lhead = l;
+    int rhead = mid+1;
+    for(int i=l;i<=r;i++){
+        if(rhead>r){
+            q.push(pointset[lhead]);
+            lhead++;
+        }else if(lhead>mid){
+            q.push(pointset[rhead]);
+            rhead++;
+        }else{
+        if(pointset[lhead].second<pointset[rhead].second){
+            q.push(pointset[lhead]);
+            lhead++;
+        }else{
+            q.push(pointset[rhead]);
+            rhead++;
+        }
+        }
     }
-    int lind = mid;
-    while(pointset[lind].first>=xmid-d && lind>l){
-        lind-=1;
+    long long mincd=LONG_LONG_MAX;
+    for(int i=l;i<=r;i++){
+        pointset[i] = q.front();
 
+        q.pop();
     }
-    long long mincd = __LONG_LONG_MAX__;
-    for(int i = lind;i<=mid;i++){
-        for(int j=mid+1;j<=rind;j++){
-            mincd = min(mincd,dis(pointset[i].first,pointset[i].second,pointset[j].first,pointset[j].second));
+    for(int i=l;i<=r;i++){
+        if(abs(pointset[i].first-xmid)<=d){
+            int j=1;
+        while(pointset[i+j].second-pointset[i].second<=d && i+j<=r){
+            if(abs(pointset[i+j].first-xmid)<=d && i+j<=r){
+                mincd = min(mincd,dis(pointset[i].first,pointset[i].second,pointset[i+j].first,pointset[i+j].second));
+            }
+            j++;
+        }
+        j=-1;
+        while(pointset[i+j].second-pointset[i].second>=-d && i+j>=l){
+            if(abs(pointset[i+j].first-xmid)<=d && i+j>=l){
+                mincd = min(mincd,dis(pointset[i].first,pointset[i].second,pointset[i+j].first,pointset[i+j].second));
+            }
+            j--;
+        }
         }
     }
     return min(mincd,min(left,right));
+
 }
 int main(){
     int n;cin>>n;
