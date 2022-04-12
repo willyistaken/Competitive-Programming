18ffea81ed4b18c6fa99455657dc32aa5757fc97
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-04-12 18:46:39 +0800

    4/12

--- sprout/6-127.cpp
+++ sprout/6-127.cpp
@@ -1,7 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+/* 
+ vector<vector<int> > grid;
+int GetVal(int p,int q){
+    return grid[p-1][q-1];
+}
+void Report(int a){
+    cout<<a<<endl;
+}  */
 void mysolve(int t,int d,int l,int r){
-    if(r-l<1) return;
+    if(d-t==1){
+        int maxv = INT_MIN;
+        int maxind=0;
+        for(int i=l;i<=r;i++){
+            int querry = GetVal(t,i);
+            if(querry>maxv){
+                maxv = querry;
+                maxind = i;
+            }
+        }
+        Report(maxind);  
+        maxv = INT_MIN;
+        for(int i=l;i<=r;i++){
+            int querry = GetVal(d,i);
+            if(querry>maxv){
+                maxv = querry;
+                maxind = i;
+            }
+        }
+        Report(maxind);
+        
+    }else if(d==t){
+        int maxv = INT_MIN;
+        int maxind = 0;
+        for(int i=l;i<=r;i++){
+            int querry = GetVal(d,i);
+            if(querry>maxv){
+                maxind = i;
+                maxv = querry;
+            }
+        }
+        Report(maxind);
+    } else{
     int mid = (t+d)/2;
     int maxind=0;int maxv=INT_MIN;
     for(int i= l;i<=r;i++){
@@ -11,10 +51,23 @@ void mysolve(int t,int d,int l,int r){
             maxv=querry;
         } 
     }
-    mysolve(t,mid,l,maxind-1);
-    Report(maxv);
+    mysolve(t,mid-1,l,maxind-1);
+    Report(maxind);
     mysolve(mid+1,d,maxind+1,r);
-};
+    }
+}
 void solve(int n,int m){
     mysolve(1,n,1,m);
 }
+/* int main(){
+    int n,m;cin>>n>>m;
+    for(int i=0;i<n;i++){
+        vector<int> te;
+        for(int j=0;j<m;j++){
+            int x;cin>>x;
+            te.push_back(x);
+        }
+        grid.push_back(te);
+    }
+    solve(n,m);
+} */
