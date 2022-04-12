168250374dc1f0d28b4c3a4153e0819ef00287e9
willyistaken <willyhlchan@gmail.com>
2022-04-11 21:51:42 -0400

    4/12

--- sprout/6-795.cpp
+++ sprout/6-795.cpp
@@ -3,13 +3,6 @@ using namespace std;
 long long dis(int x,int y,int ax,int ay){
     return ((x-ax)*(x-ax))+((y-ay)*(y-ay));
 }
-int bs(vector<pair<int,int> > &arr,int key,bool type){
-    if(type){
-    
-    }else{
-
-    }
-}
 long long solve(vector<pair<int, int> > &pointset,int l,int r){
     if(r==l+1){
         return dis(pointset[r].first,pointset[r].second,pointset[l].first,pointset[l].second);
@@ -19,12 +12,19 @@ long long solve(vector<pair<int, int> > &pointset,int l,int r){
     long long right= solve(pointset,mid+1,r);
     int xmid = (pointset[mid].first+pointset[mid+1].first)/2;
     int d = ceil(sqrt(min(left,right)) );
-    int rind = upper_bound(pointset.begin(), pointset.end() , xmid-d , [](int a, pair<int,int> b){return a<= b.first;})-pointset.begin();
-    int lind = upper_bound(pointset.begin(),pointset.end(),xmid+d,  [](int a, pair<int,int> b){return a>= b.first;})-pointset.begin();
-    long long mincd=LONG_LONG_MAX;
-    for(int i =rind;i<=lind;i++){
+    int rind = mid+1;
+    while(pointset[rind].first<xmid+d){
+        rind+=1;
+    }
+    int lind = mid;
+    while(pointset[lind].first<xmid-d){
+        lind+=1;
+
+    }
+    long long mincd = __LONG_LONG_MAX__;
+    for(int i = rind;i<=lind;i++){
         for(int j=i+1;j<=lind;j++){
-             mincd  = min(mincd,dis(pointset[i].first,pointset[i].second,pointset[j].first,pointset[j].second));
+            mincd = min(mincd,dis(pointset[i].first,pointset[i].second,pointset[j].first,pointset[j].second));
         }
     }
     return min(mincd,min(left,right));
