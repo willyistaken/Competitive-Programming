54e98c1b5e73b3e85301a5c042d32c6be3eef1ef
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-04-20 14:33:18 +0800

    4/20

--- sprout/7-357.cpp
+++ sprout/7-357.cpp
@@ -1,66 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n,m,r;
 
-long long damage(int s,int h,vector<pair<int,int> > &spells,int n,int m){
-    //binary search for h:
-    int l=-1;int r=n-1;
-    while(r-l>1){
-        int mid = (l+r)/2;
-        if(spells[mid].first>(h/m)){
-            r=mid;
-        }else{
-            l=mid;
-        }
+int nextPowerOf2(int n) 
+{
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
+
+void modify(int ind,int k,pair<int,int>* segtree){
+    int realloc = ind+nextPowerOf2(n);
+    segtree[realloc] = make_pair(k,ind);
+    while(realloc!=1){
+        realloc/=2;
+        segtree[realloc]=max(segtree[realloc*2],segtree[(realloc*2)+1]);
     }
-    //find the sum of the s_th largest c:
-    if(n-r>=s){
-        long long sum=0;
-        for(int i=r;i<n;i++){
-            sum+=spells[i].second;
-        }
-        return sum;
-    }else{
-        long long sum=0;
-        int temparr[n-r];
-        for(int i=r;i<n;i++){
-            temparr[i-r]=spells[i].second;
-        }
-        nth_element(temparr,temparr+s-1,temparr+n-r);
-        for(int i=n-r-1;i>=n-r-s;i--){
-            sum+=temparr[i];
-        }
-        return sum;
-    }
+
+}
+pair<int,int> query(int ind,int l,int r,int ql,int qr, pair<int,int>* segtree){
+    
+    if(qr<ql || r<l) return make_pair(INT_MIN,-1);
+    if(r<ql||l>qr) return make_pair(INT_MIN,-1);
+    if(l>=ql && r<=qr) return segtree[ind];
+    if(qr==ql) return segtree[r+nextPowerOf2(n)];
+    int mid = (l+r)/2;
+    return max(query(ind*2,l,mid,ql,qr,segtree),query(ind*2+1,mid+1,r,ql,qr,segtree));
 }
 
 int main(){
-    int n,m,r;cin>>n>>m>>r;
-    vector<pair<int,int> > spells(n,make_pair(0,0));
+    cin>>n>>m>>r;
+    vector<pair<int,int> > spells(n);
     for(int i=0;i<n;i++){
         int p,c;cin>>p>>c;
         spells[i]=make_pair(p,c);
     }
     sort(spells.begin(),spells.end());
-    long long dp[2]={m,m};
-    bool stop=false;
-    int i=1;
-    while(!stop){
-        dp[i%2]=min(dp[(i-1)%2]-damage(i,dp[(i-1)%2],spells,n,m)+r,(long long)m);
-        if(i>n+1 && dp[0]==dp[1]){
-            stop=1;
-            continue;
-        }
-        cout<<dp[i%2]<<"+"<<damage(i,dp[(i-1)%2],spells,n,m)<<endl;
-        if(dp[i%2]<=0){
-            break;
-        }
-        i++;
+    pair<int,int> segtree[2*nextPowerOf2(n)]={make_pair(0,-1)};
+    for(int i=0;i<n;i++){
+        modify(i,spells[i].second, segtree );
     }
-    if(stop){
+    long long i=0;
+    long long health=m;
+    long long cdamage=0;
+    while(!(i>n && cdamage<=r)){
+        health-=cdamage;
+        health+=r;
+        health=min(health,(long long) m);
+        //binary search for h
+        int l=-1;int r=n;
+        while(r-l>1){
+            int mid=(r+l)/2;
+            if((double)spells[mid].first>=(double)(health*100.0)/(double)m){
+                r=mid;
+            }else{
+                l=mid;
+            }
+        }
+        // binary search for h ----code-is-trash---code-is-trash---- 
+        if(health<=0) break;
+        i++;
+        if(r>=n) continue;
+        pair<int,int> temp=query(1,0,n-1,r,n-1,segtree);
+        
+        if((temp.first!=-1)){
+            cdamage+=temp.first;
+            modify(temp.second,-1,segtree);
+        }       
+       
+    }
+    if((i>n && cdamage<=r)){
         cout<<-1<<endl;
     }else{
         cout<<i<<endl;
     }
+
 
 
 }
---
+++ template/segment_tree.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//input: n(the size of the array) follow by n interger (stuff in the array)
+vector<int> arr;
+int n;
+
+int nextPowerOf2(int n) 
+{
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
+void modify(int ind,int k,vector<int> &segtree){
+    int realind = ind+nextPowerOf2(n);
+    segtree[realind]=k;
+    while(realind!=1){
+        realind/=2;
+        segtree[realind] = max(segtree[realind*2],segtree[realind*2+1]);
+    }
+}
+int query(int cur,int l,int r,int ql,int qr,vector<int> &segtree){
+    if(ql>qr || l>r) return INT_MIN;
+    if(r<ql || l>qr) return INT_MIN;
+    if(l>=ql && r<=qr) return segtree[cur];
+    if(r==l) return segtree[l+nextPowerOf2(n)];
+    int mid = (l+r)>>1;
+    return max(query(cur*2,l,mid,ql,qr,segtree),query(cur*2+1,mid+1,r,ql,qr,segtree));
+}
+void travel(int i,vector<int> &segtree){
+    if(i>=2*nextPowerOf2(n)) return;
+    cout<<segtree[i]<<endl;
+    travel(2*i,segtree);
+    travel(2*i+1,segtree);
+}
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int temp;cin>>temp;
+        arr.push_back(temp);
+    }
+    vector<int> segtree(2*nextPowerOf2(n),-1);
+    
+    for(int i=0;i<n;i++){
+        modify(i,arr[i],segtree);
+    }
+    for(int i=0;i<5;i++){
+        cout<<query(1,0,n-1,0,i,segtree)<<endl;
+    }
+
+
+}
