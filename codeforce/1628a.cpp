15333b1a06623479286ba6c563179e985fe7a6fc
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-04-23 21:53:32 +0800

    save

--- codeforce/1628a.cpp
+++ codeforce/1628a.cpp
@@ -1,56 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-bitset<200005>ha;
-struct pa{
-    int locate;
-    int vaule;
-};
-pa mex(int l,int r,int arr[]){
-   ha.reset();
-    int m=0;
-    int ans=l;
-    int last=0;
-    for(int i=l;i<=r;i++){
-       ha.set(arr[i]);
-        if(ha[m]){
-            while(ha[m]) m++;
+int t, n;
+int a[220001];
+queue<int> q[220001];
+vector<int> ans;
+int main() {
+    cin >> t;
+    while (t--) {
+        ans.clear();
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            q[a[i]].push(i);
         }
-        if(m!=last) ans=i;
-        last=m;
+        int l = 1, r = 0;
+        while (l <= n) {
+            r = l;
+            for (int i = 0; i <= n + 1; i++) {
+                if (q[i].empty()) {
+                    ans.push_back(i);
+                    break;
+                }
+                r = max(r, q[i].front());
+            }
+            for (int i = l; i <= r; i++) {
+                q[a[i]].pop();
+            }
+            l = r + 1;
+        }
+        cout << ans.size() << endl;
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << ' ';
+        }
+        cout << endl;
     }
-    pa par;
-    par.locate=ans;
-    par.vaule=last;
-    return par;
-}
-int main(){
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    int t;cin>>t;
-    while(t--){
-       int n;cin>>n;
-    int ans=0;
-    int array[n];
-    for(int i=0;i<n;i++){
-        cin>>array[i];
-    }
-    queue<int> reans;
-    pa l;
-    l.locate=-1;
-    l.vaule=0;
-    while(l.locate<n-1 ){
-        l=mex(l.locate+1,n-1,array);
-        ans++;
-        reans.push(l.vaule);
-    }
-    cout<<ans<<endl; 
-    while(!reans.empty()){
-        cout<<reans.front()<<" ";
-        reans.pop();
-    }
-    cout<<"\n";
-    }
-    
 }
