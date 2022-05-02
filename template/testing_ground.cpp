0bf573aaaa95c98e52f9abf189a4a4574cadf3b0
willyistaken <willyhlchan@gmail.com>
2022-05-02 06:30:03 -0400

    what?

--- template/testing_ground.cpp
+++ template/testing_ground.cpp
@@ -1,23 +1,51 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long;
-int f(int n){
-	int sum=0;
-	if(n<=0) return 0;
-	if(n==1) return 1;
-	sum+=f(n-1);
-	sum+=f(n-2);
-	return sum;
+#include<cstdio>
+inline char readchar(){
+	const int S = 1<<14;
+	static char buf[S], *p = buf, *q = buf;
+	if(p==q and (q=(p=buf)+fread(buf,1,S,stdin))==buf) return EOF;
+	return *p++;
 }
+inline int readint(){
+	int ans = 0, c = readchar();
+	while((c<'0' or c>'9')and c!=EOF) c=readchar();
+	while(c>='0' and c<='9') ans=(ans<<3)+(ans<<1)+(c^'0'), c=readchar();
+	return ans;
+}
+
+int n, m, c, ans, thresh,
+	dx[8] = {1, 0, -1, 1, -1, 1, 0, -1},
+	dy[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+bool g[1002][1002];
+inline bool in(int i, int j){
+	return i >= 0 and j >= 0 and i < n and j < m;
+}
+void dfs(int i, int j){
+	g[i][j] = 0;
+	for(int k = 0; k < 8; ++k)
+		if(g[i+dx[k]][j+dy[k]])
+			dfs(i+dx[k], j+dy[k]);
+}
+
 int main(){
-	f(4);
-
-
-
-
-
-
-
-
+	while(true){
+		n = readint(), m = readint();
+		if(n==0) break;
+		thresh = n*m>>2, ans = 0;
+		for(int i = 1; i <= n; ++i){
+			for(int j = 1; j <= m; ++j){
+				do{
+					c = readchar();
+				}while(c < '0');
+				g[i][j] = (c=='1');
+			}
+		}
+		for(int i = 1; i <= n; ++i){
+			for(int j = 1; j <= m; ++j){
+				if(g[i][j]) ++ans, dfs(i, j);
+				if(ans > thresh) break;
+			}
+		}
+		printf("%d\n", ans);
+	}
 	return 0;
 }
---
+++ tioj/1081.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+int dx[8]={0,0,1,1,1,-1-1-1};
+int dy[8]={1,-1,0,1,-1,0,1,-1};
+bool inbound(int x,int y){
+    if(x<n&&x>=0){
+        if(y<m&&y>=0){
+            return true;
+        }
+    }
+    return false;
+}
+void dfs(int x,int y,vector<vector<bool>> &arr){
+    for(int i=0;i<8;i++){
+        if(inbound(x+dx[i],y+dy[i])){
+            if(arr[x+dx[i]][y+dy[i]]){
+                arr[x+dx[i]][y+dy[i]]=0;
+                dfs(x+dx[i],y+dy[i],arr);
+            }
+        }
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    while(cin>>n>>m && n && m){
+        int upper = n*m>>2;
+    vector<vector<bool> > arr(n,vector<bool>(m,0));
+    for(int i=0;i<n;i++){
+        string k;cin>>k;
+        for(int j=0;j<m;j++){
+        arr[i][j]=(k[j]=='1');
+        }
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j]){
+                arr[i][j]=0;
+                ++ans;
+                dfs(i,j,arr);
+            }
+            if(ans>upper) break;
+            
+        }
+    }
+    cout<<ans<<"\n";
+    }
+}
