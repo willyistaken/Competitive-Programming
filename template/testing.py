56c48bb0644050e2bc2808f9e4ad92f08fbc4900
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-05-09 21:34:49 +0800

    1051

--- template/testing.py
+++ template/testing.py
@@ -1,16 +1,14 @@
-import re
-import string
-
-def lookandsay(limit, sequence = 1):
-
-    if limit > 1:
-
-        return lookandsay(limit-1, "".join([str(len(match.group()))+match.group()[0] for matchNum, match in enumerate(re.finditer(r"(\w)\1*", str(sequence)))]))
-
-    else:
-
-        return sequence
+import math
+import random
+sum=0
+n=100000
+for i in range(n):
+    a=random.randint(1,n)
+    b=random.randint(1,n)
+    sum+=abs(a-b)
+print(sum/n)
 
 
-for i in range(1,44):
-    print("\"{} {:.6f}\\n\",".format(len(str(lookandsay(i))),len(str(lookandsay(i-1)))/len(str(lookandsay(i)))))
+
+
+
--- tioj/1051.cpp
+++ tioj/1051.cpp
@@ -1,184 +1,73 @@
-#include <bits/stdc++.h>//this is a LIS + LCS;
+//LDCS
+//dp[i][j] consider the first array up to i_th element and need to use the j_th element of the second array
+//dp[i][j] = dp[i-1][j] (if a[i]!=b[j])
+//         = max(dp[i-1][k])+1 where 1<=k<j and b[k]>=b[j] (a[i]==b[j])
+//#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
 using namespace std;
-struct store{
-    int value;
-    vector<int> arr;
-};
-inline pair<int,char> mymax(pair<int,char> &a,pair<int,char> &b,pair<int,char> &c){
-    if(a>b){
-        if(b>=c){
-         return {a.first,2};
-        }else {
-            if(c>a) return {c.first,4};
-            return {a.first,2};
-        }
-    }else{
-        if(a>=c){
-            return {b.first,3};
-        }else{
-            if(c>b) return {c.first,4};
-            return {b.first,3};
-        }
-    }
-}
-int dx[4]={-1,-1,0,-1};
-int dy[4]={-1,0,-1,-1};
-int  main(){
-
+#define ll long long;
+int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t;cin>>t;
     while(t--){
-
     int n;cin>>n;
-    //LIS
-    vector<int> ans1;
-    vector<int> ans2;
-    vector<store> s;
+    int a[n+1],b[n+1];
+    int dp[n+1][n+1] ={{0}};
+    int path[n+1][n+1];
     for(int i=0;i<n;i++){
-       int a;cin>>a;
-       vector<int> temp;
-       store tempstore;
-       tempstore.value=a;
-       tempstore.arr=temp;
-       auto it = upper_bound(s.begin(),s.end(),tempstore,[](const store &a,const store &b){ return a.value>b.value ;});
-       if(it!=s.end()){
-           s[it-s.begin()]=tempstore;
-           if(it-s.begin()){
-               (s[it-s.begin()]).arr=(s[it-s.begin()-1]).arr;
-           }
-           ((s[it-s.begin()]).arr).push_back(a);
-       }else{
-           s.push_back(tempstore);
-           if(s.size()!=1){
-               (s[s.size()-1]).arr=(s[s.size()-2]).arr;
-           }
-           (s[s.size()-1].arr).push_back(a);
-       }
-        
+        cin>>a[i+1];
     }
-    ans1=s[s.size()-1].arr;
-
     for(int i=0;i<n;i++){
-       int a;cin>>a;
-       vector<int> temp;
-       store tempstore;
-       tempstore.value=a;
-       tempstore.arr=temp;
-       auto it = upper_bound(s.begin(),s.end(),tempstore,[](const store &a,const store &b){ return a.value>b.value ;});
-       if(it!=s.end()){
-           s[it-s.begin()]=tempstore;
-           if(it-s.begin()){
-               (s[it-s.begin()]).arr=(s[it-s.begin()-1]).arr;
-           }
-           ((s[it-s.begin()]).arr).push_back(a);
-       }else{
-           s.push_back(tempstore);
-           if(s.size()!=1){
-               (s[s.size()-1]).arr=(s[s.size()-2]).arr;
-           }
-           (s[s.size()-1].arr).push_back(a);
-       }
-        
+        cin>>b[i+1];
     }
-    ans2=s[s.size()-1].arr;
     
-    //LCS
-    pair<int,char> dp[ans1.size()+1][ans2.size()+1];
-    for(int i=0;i<=ans1.size();i++){
-        for(int j=0;j<=ans2.size();j++){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
             if(i==0 || j==0){
-                dp[i][j]={0,0};
-            }else{
-                if(ans1[i-1]==ans2[j-1]){
-                    dp[i][j]={dp[i-1][j-1].first+1,1};
-                }else{
-                    dp[i][j] = mymax(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]);
-                }
+                dp[i][j]=0;
+                path[i][j]=-1;
+                continue;
             }
+            if(a[i]!=b[j]){
+                dp[i][j]=dp[i-1][j];
+                path[i][j]=j;
+            }else{
+                int maxn=0;
+                int maxid=-1;
+                for(int g=j-1;g>=1;g--){
+                    if(b[g]>=b[j]){
+                        if(maxn<dp[i-1][g]){
+                            maxn=dp[i-1][g];
+                            maxid=g;                          
+                        }
+                    }
+                }
+                dp[i][j]=maxn+1;
+                path[i][j]=maxid;
+            }
+            //cout<<path[i][j]<<" ";
         }
+        //cout<<endl;
     }
-    int x=ans1.size();int y=ans2.size();
-    vector<int> realans;
-    while(dp[x][y].first){
-        if(dp[x][y].second==(char)1) realans.push_back(ans1[x-1]);
-            int chari = dp[x][y].second;
-            x+=dx[chari-1];
-            y+=dy[chari-1];
+    int maxnn=0;
+    int indn=-1;
+    for(int i=0;i<=n;i++){
+        if(maxnn<dp[n][i]){
+            maxnn=dp[n][i];
+            indn=i;
+        }
+   } 
+    stack<int> ans;
+    for(int i=n;i>0;i--){
+        if(indn==-1) break;
+        if((path[i][indn]<indn && dp[i][indn]-dp[i-1][path[i][indn]]==1) || path[i][indn]==-1)  ans.push(b[indn]);
+        indn=path[i][indn];
     }
-    for(auto i=realans.rbegin();i!=realans.rend();i++){
-        cout << (*i) <<" \n"[next(i)==realans.rend()];
+    if(ans.empty()) cout<<endl;//note to myself: this line is the freaking reason it doesn't ac, because it needed endl even if the length of LCDS is 0. :(
+    while(!ans.empty()){
+        cout<<ans.top()<<" \n"[ans.size()==1];
+        ans.pop();
     }
     }
+    return 0;
 }
-/*
-
-#include <stdio.h>
-
-int c, dp[1000005], ary[1000005], ptr[1000005], last[1000005];
-
-void backtracking(int now)
-{
-	if(now == 0)
-	{
-		return;
-	}
-	backtracking(last[now]);
-	if(c == 0)
-	{
-		printf("%d", ary[ptr[now]]);
-		c++;
-	}
-	else
-	{
-		printf(" %d", ary[ptr[now]]);
-	}
-}
-
-int main()
-{
-	int count, n, i, j, t, temp, maxlen, ans;
-	scanf("%d", &count);
-	while(count--)
-	{
-		scanf("%d", &n);
-		for(i=1; i<=n; i++)
-		{
-			scanf("%d", &ary[i]);
-		}
-		maxlen = 0;
-		dp[0] = 0;
-		ptr[0] = 0;
-		ary[0] = 1000000;
-		for(i=1, maxlen=0, ans=0; i<=n; i++)
-		{
-			scanf("%d", &temp);
-			for(j=1; j<=n&&ary[j]!=temp; j++);
-			if((t=j) <= n)
-			{
-				ptr[i] = t;
-				for(j=i-1, dp[i]=0; j>=0; j--)
-				{
-					if(ptr[j] != -1 && temp < ary[ptr[j]] && ptr[j] < t && dp[j] + 1 > dp[i])
-					{
-						dp[i] = dp[j] + 1;
-						last[i] = j;
-					}
-				}
-				if(dp[i] > maxlen)
-				{
-					maxlen = dp[i];
-					ans = i;
-				}
-			}
-			else
-			{
-				ptr[i] = -1;
-			}
-		}
-		c = 0;
-		backtracking(ans);
-		printf("\n");
-	}
-	return 0;
-}
-*/
