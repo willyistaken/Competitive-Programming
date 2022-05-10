122c225574fd8333497e1eca8032ca5a242d3be2
willyistaken <willyhlchan@gmail.com>
2022-05-10 12:55:37 +0000

    5/10

---
+++ tioj/1217.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+
+int main(){
+	ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	string s;
+	while(getline(cin,s)){
+		cout<<s<<endl;	
+	}
+
+	return 0;
+}
