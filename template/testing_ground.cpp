758ba5a9040e7a618f73bee359483d5a41c4e96e
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-05-18 18:14:07 +0800

    5/18

--- template/testing_ground.cpp
+++ template/testing_ground.cpp
@@ -2,10 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long;
+bool eval(bool a,bool b,bool c,bool d){
+	return (a || !d)&&(!a || b)&&(!b || c)&&(!a || !b || !c)&&(a || !c || d);
+}
 int main(){
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	for(int i=0;i<100;i++) cout<<i<<__lg(i&-i)<<endl;
-
+	for(int i=0;i<16;i++){
+		if(eval(i&1,(i>>1)&1,(i>>2)&1,(i>>3)&1)){
+			cout<<i;
+		}
+	}
 
 
 
