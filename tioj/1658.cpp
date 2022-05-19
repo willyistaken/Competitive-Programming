1c7cf07a1117d2b02e8b05c8de1405ffec7cbec1
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-05-19 23:14:00 +0800

    save

---
+++ tioj/1658.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+inline void white(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<2*n-1;j++){
+            if(abs((j-n+1))==i || i==n-1){
+                putchar('*');
+            }else{
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+    }
+}
+
+inline void black(int n){
+        for(int i=0;i<n;i++){
+        for(int j=0;j<2*n-1;j++){
+            if(abs((j-n+1))<=i || i==n-1){
+                putchar('*');
+            }else{
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+    }
+}
+
+int main(){
+
+    int n;int type;
+    while(scanf(" %d %d",&n,&type)!=EOF){
+        if(type){
+            black(n);
+        }else{
+            white(n);
+        }
+    }
+
+
+
+
+
+
+
+    return 0;
+}
