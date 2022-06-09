a35a0f103fac3db925f2904d164ec58b51364946
willyistaken <willyhlchan@gmail.com>
2022-06-09 20:48:09 +0800

    6/9

--- tioj/1297.cpp
+++ tioj/1297.cpp
@@ -1,32 +1,27 @@
-#pragma GCC optimize("Ofast")
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define ll long long
+
+
+
+int arr[1050];
 int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int n;
-   while(cin>>n){
-       if(n<0) {
-           cout<<"stupid\n";
-           continue;
-       }
-       for(int i=0;i<n;i++){
-           if(i == ((n-i)/100)){
-               if(n-i>1024 || n-i<0){
-                   cout<<"stupid\n";
-               }else{
-                   cout<<n-i<<endl;
-               }
-               break;
-           }
-       }
-   } 
-
-
-
-
-
-
-
-    return 0;
+	ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	for(int i=1;i<=1024;i++){
+		arr[(i*101)/100]=i;	
+	}
+	int x;
+	while(cin>>x){
+		if(x<0 || x>1035){
+			cout<<"stupid\n";	
+		}else{
+			if(x){
+				if(arr[x]) cout<<arr[x]<<endl;
+				else cout<<"stupid\n";
+			}else{
+				cout<<0<<endl;	
+			}
+		}
+	}
+	return 0;
 }
