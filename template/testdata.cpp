d8e8e3f30efcc5ebc39d99c8e635c961defea49c
willyistaken <willyhlchan@gmail.com>
2022-09-30 17:04:14 +0800

    9/30

--- template/testdata.cpp
+++ template/testdata.cpp
@@ -7,9 +7,18 @@ typedef long long ll;
 int main(){
 	srand( time(NULL) );
 	ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-	int n = rand()%15;
-	cout<<n<<"\n";
-	for(int i=0;i<n;i++) cout<<rand()%10000000<<" ";
-	cout<<"\n";
-	return 0;
+	int n = 10;
+	int k = 0;
+	int M = 1e9+1;
+	int l = rand()%M;
+	int r = rand()%M;
+	if(l>r) swap(l,r); 
+	cout<<n<<" "<<k<<"\n";
+	int dig[n];
+	for(int i=0;i<n;i++) dig[i]=i;
+	random_shuffle(dig,dig+n);
+	for(int i=0;i<k;i++){
+		cout<<dig[i]<<" \n"[i==k-1];	
+	}
+	cout<<l<<" "<<r<<"\n";
 }
