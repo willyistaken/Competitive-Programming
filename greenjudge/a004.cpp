4110640a14de7f31131ab06fd6e74b13db9c5b7d
willyistaken <willyhlchan@gmail.com>
2022-10-10 14:49:47 +0800

    10/10

--- greenjudge/a004.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a;cin>>a;
-    cout<<a+1911<<endl;
-    return 0;
-}
-//asdfasd
--- greenjudge/a005.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int h,w;cin>>h>>w;
-    cout<<h*w<<endl;
-    return 0;
-}
