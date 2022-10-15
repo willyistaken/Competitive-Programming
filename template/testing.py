8f1f04fe7ecd7096e909897df58b5b2215dc4658
willyistaken <willyhlchan@gmail.com>
2022-10-15 10:17:36 +0800

    10/15

--- template/testing.py
+++ template/testing.py
@@ -1,22 +1,61 @@
-def A000110_list(m):
+#!/usr/bin/env python3
 
-    A = [0 for i in range(m)]
+# This program prints all hcn (highly composite numbers) <= MAXN (=10**18)
+#
+# The value of MAXN can be changed arbitrarily. When MAXN = 10**100, the
+# program needs less than one second to generate the list of hcn.
+from math import log
 
-    A[0] = 1
+MAXN = 2**32
 
-    R = [1, 1]
+# Generates a list of the first primes (with product > MAXN).
+def gen_primes():
+	primes = []
+	primes_product = 1
+	for n in range(2, 10**10):
+		is_prime = True
+		for i in range(2, n):
+			if n % i == 0:
+				is_prime = False
+		if is_prime: 
+			primes.append(n)
+			primes_product *= n
+			if primes_product > MAXN: break
+	return primes
+primes = gen_primes()
 
-    for n in range(1, m):
+# Generates a list of the hcn <= MAXN.
+def gen_hcn():
+    # List of (number, number of divisors, exponents of the factorization)
+	hcn = [(1, 1, [])]
+	for i in range(len(primes)):
+		new_hcn = []
+		for el in hcn:
+			new_hcn.append(el)
+			if len(el[2]) < i: continue
+			e_max = el[2][i-1] if i >= 1 else int(log(MAXN, 2))
+			n = el[0]
+			for e in range(1, e_max+1):
+				n *= primes[i]
+				if n > MAXN: break
+				div = el[1] * (e+1)
+				exponents = el[2] + [e]
+				new_hcn.append((n, div, exponents))
+		new_hcn.sort()
+		hcn = [(1, 1, [])]
+		for el in new_hcn:
+			if el[1] > hcn[-1][1]: hcn.append(el)
+	return hcn
 
-        A[n] = A[0]
 
-        for k in range(n, 0, -1):
+hcn = gen_hcn()
+# From here on is only pretty printing.
+print("Number of highly composite numbers less than", MAXN, "is", len(hcn),"\n")
 
-            A[k-1] += A[k]
 
-        R.append(A[0])
 
-    return R
 
-p = A000110_list(10000)
-print(p)
+for el in hcn:
+	factorization = "*".join([str(primes[i])+("^"+str(el[2][i]) if el[2][i]>1 else "") for i in range(len(el[2]))])
+	
+	print(str(el[0]))
--- tioj/1695.cpp
+++ tioj/1695.cpp
@@ -5,19 +5,60 @@ typedef long long ll;
 //__gnu_pbds::
 
 
+ll phi(ll k){
+	ll r = k;
+	for(ll p = 2;p*p<=k;p++){
+		if(k%p==0){
+			while(k%p==0) k/=p;
 
+			r-= r/p;
+		}
+	}
+	if(k>1) r-=r/k;
+	return r;
+}
+ll n,m;
+ll pow(ll p){
+	ll r = 1;
+	ll a = n;
+	while(p){
+		if(p & 1) r = (r*a)%m;
+		a = (a*a)%m;
+		p>>=1;
+	}
+	return r;
+}
 
 int main(){
 	ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-	int m,n;cin>>n>>m;
-	int r= 1;
-	int ans=0;
-	for(int i=1;i<m;i++) if(__gcd(i,m)==1) ans+=1;
-	cout<<ans<<"\n";
-	for(int i=0;i<=100;i++){
-		cout<<i<<" "<<r<<"\n";
-		r = (1LL*n*r)%m;
+	while(cin>>n>>m){
+		ll d = __gcd(pow(m),m);
+		ll l = -1;ll r = m;
+		while(r-l>1){
+			ll mid = l +((r-l)/2);
+			if(__gcd(pow(mid),m)==d) r = mid;
+			else l = mid;
+		}
+		ll x0 = l+1;
+		ll start = pow(x0);
+		ll p = phi(m);
+		ll mincycle = p;
+		for(ll i = 1;i<=sqrt(p);i++){
+			if(p%i==0){
+				if(pow(x0+i)==start) {
+					mincycle = min(mincycle,i);
+					break;
+				}
+				if(pow(x0+(p/i))==start) mincycle = min(mincycle,p/i);
+			}
+		}
+		cout<<x0<<" "<<mincycle<<"\n";
 	}
-
 	return 0;
 }
+
+
+/*
+idea now: use the fact the gcd will be stable, binary search for x, and use the fact that phi(m) must be a mutiple of p, search for p;
+too slow, I guess the bottle neck is at phi and factor (sqrt(n)...)
+*/
